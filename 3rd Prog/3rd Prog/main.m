a6dde66192375273c685c000230cbc3004890d95
Rajesh <rajeshbobba@yahoo.com>
2014-05-15 13:25:25 -0400

    3rd Prog

---
+++ 3rd Prog/3rd Prog/main.m
@@ -0,0 +1,33 @@
+//
+//  main.m
+//  3rd Prog
+//
+//  Created by User1 on 2014-05-15.
+//  Copyright (c) 2014 Rajesh bobba. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        int n, n2,i ;
+        
+        NSLog(@"n     n2");
+        
+        
+        for(i=1;i <=10; ++i) {
+            
+            n = i;
+            
+            n2=i;
+            
+            
+            NSLog(@"%i      %i", n, n2);
+            
+        }
+    }
+    return 0;
+}
+
